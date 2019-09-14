80a25c7e2ce59735cf6ca956b98200edb2510823
Adham  Hamed <hameda6256@gmail.com>
2019-09-14 04:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,37 +2,34 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
+function destructivelyAppendKitten(name){
+    return kittens.push(name)
 }
 
-function destructivelyPrependKitten(name) {
-  kittens.unshift(name)
-  return kittens
+function destructivelyPrependKitten(name){
+    return kittens.unshift(name)
 }
 
-function destructivelyRemoveLastKitten() {
-  return kittens.pop()
+function destructivelyRemoveLastKitten(){
+    return kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
+function destructivelyRemoveFirstKitten(){
+    return kittens.shift()
 }
 
-function appendKitten(name) {
-  return [...kittens, name]
+function appendKitten(name){
+    return [...kittens, name]
 }
 
-function prependKitten(name) {
-  return [name, ...kittens]
+function prependKitten(name){
+    return [name, ...kittens]
 }
 
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
 }
 
-function removeFirstKitten() {
-  return kittens.slice(1)
+function removeFirstKitten(){
+    return kittens.slice(1)
 }
- 0  package.json  100644 → 100755
